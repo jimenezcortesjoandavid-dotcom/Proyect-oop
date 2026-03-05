515731d121ef4c6bdf2b6e0d90d9655daeb834df
Jimenex <jimenezcortesjoandavid@gmail.com>
2026-03-04 22:07:17 -0500

    project idea, documentation, and Rust setup

---
+++ First update/test1/target/debug/deps/prueba1.d
@@ -0,0 +1,5 @@
+C:\Users\User\OneDrive\Documents\COSAS\Proyects oop\First update\test1\target\debug\deps\prueba1.d: src\main.rs
+
+C:\Users\User\OneDrive\Documents\COSAS\Proyects oop\First update\test1\target\debug\deps\prueba1.exe: src\main.rs
+
+src\main.rs:
---
+++ First update/test1/target/debug/prueba1.d
@@ -0,0 +1,1 @@
+C:\Users\User\OneDrive\Documents\COSAS\Proyects\ oop\First\ update\test1\target\debug\prueba1.exe: C:\Users\User\OneDrive\Documents\COSAS\Proyects\ oop\First\ update\test1\src\main.rs
